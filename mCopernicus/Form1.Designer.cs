d36f844051d6b233318c5564cf0bd45afa7d2acd
Milkey Tan <819774989@qq.com>
2017-06-12 11:28:30 +0800

    基本

--- mCopernicus/Form1.Designer.cs
+++ mCopernicus/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.addButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.startButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SuspendLayout();
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(1, 540);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(400, 1);
+            this.materialDivider1.TabIndex = 0;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // addButton
+            // 
+            this.addButton.AutoSize = true;
+            this.addButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.addButton.Depth = 0;
+            this.addButton.Icon = global::mCopernicus.Properties.Resources.plus;
+            this.addButton.Location = new System.Drawing.Point(13, 550);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.addButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.addButton.Name = "addButton";
+            this.addButton.Primary = false;
+            this.addButton.Size = new System.Drawing.Size(44, 36);
+            this.addButton.TabIndex = 2;
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.AutoSize = true;
+            this.startButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.startButton.Depth = 0;
+            this.startButton.Icon = global::mCopernicus.Properties.Resources.send;
+            this.startButton.Location = new System.Drawing.Point(303, 550);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.startButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.startButton.Name = "startButton";
+            this.startButton.Primary = false;
+            this.startButton.Size = new System.Drawing.Size(84, 36);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "启动";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(400, 600);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.materialDivider1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mCopernicus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MaterialSkin.Controls.MaterialFlatButton startButton;
+        private MaterialSkin.Controls.MaterialFlatButton addButton;
     }
 }
 
--- mCopernicus/Form1.cs
+++ mCopernicus/Form1.cs
@@ -1,20 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Diagnostics;
 
 namespace mCopernicus
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey500, Primary.BlueGrey700, Primary.BlueGrey700, Accent.Blue400, TextShade.WHITE);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Text = runSS("./shadowsocks-libqss.exe", "-T");
+        }
+
+        public static string runSS(string ssFile, string cmdStr)
+        {
+            string outPut = "";
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = ssFile;
+            startInfo.Arguments = cmdStr;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardInput = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.CreateNoWindow = true;
+            process.StartInfo = startInfo;
+            process.Start();
+            try
+            {
+                if (process.Start())//开始进程   
+                {
+                    outPut = process.StandardOutput.ReadToEnd();//读取进程的输出   
+                }
+            }
+            finally
+            {
+                if (process != null)
+                {
+                    process.Close();
+                }
+            }
+
+            return outPut;
         }
     }
 }
